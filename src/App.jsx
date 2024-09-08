67f2547f73455ad79ba9788833fb27a6e4e7d998
chasov-artem <chasov90@gmail.com>
2024-09-08 12:54:43 +0300

    third step

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import Notification from "./components/Notification/Notification";
-import React, { useState, useEffect } from "react";
-import Feedback from "./components/Feedback/Feedback";
-import Options from "./components/Options/Options"
+import { useState, useEffect } from "react";
+// import Notification from "./components/Notification/Notification";
+// import Feedback from "./components/Feedback/Feedback";
+// import Options from "./components/Options/Options";
 
-  function App = () => {
-    const [feedback, setFeedback] = useState({
-      good: 0,
-      neutral: 0,
-      bad: 0
-    });
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-    useEffect(() => {
-      const savedFeedback = JSON.parce(localStorage.getItem('feedback'));
-      if (savedFeedback) {
-        setFeedback(savedFeedback)
-      }
-    }, []
-    );
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-    useEffect(() => {
-      localStorage.setItem('feedback', JSON.stringify(feedback)), [feedback]
-    })
+  useEffect(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (savedFeedback) {
+      setFeedback(savedFeedback);
+    }
+  }, []);
 
-    
-const updateFeedback = (feedbackType) => {
-  setFeedback((prevFeedback) => ({
-    ...prevFeedback,
-    [feedbackType]: prevFeedback[feedbackType] + 1,
-  }));
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <button onClick={() => updateFeedback("good")}>Good</button>
+      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+      <button onClick={() => updateFeedback("bad")}>Bad</button>
+
+      {totalFeedback > 0 ? (
+        <div>
+          <h2>Feedback Statistics</h2>
+          <p>Good: {feedback.good}</p>
+          <p>Neutral: {feedback.neutral}</p>
+          <p>Bad: {feedback.bad}</p>
+        </div>
+      ) : (
+        <p>No feedback given yet</p>
+      )}
+    </div>
+  );
 };
-
-}
 
 export default App;
